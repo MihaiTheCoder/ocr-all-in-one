c65755ed271b9e1486f644027e1443d299f3121d
Petrutiu Mihai <petrutiu.mihai@gmail.com>
2020-05-16 09:45:33 +0300

    Remove old console self hosted OWIN (#7)
    * Remove old console self hosted OWIN
    Add ASP.NET Core API to invoke the shared library
    
    * try on windows latest
    
    * Test with msbuild
    
    * pipeline: change name from build and publish to build
    gitignore: remove test solution
    
    Co-authored-by: Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>

---
+++ Ocr.Wrapper.WebAPI/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Ocr.Wrapper.WindowsOcr;
+
+namespace Ocr.Wrapper.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<WindowsOcrExecutor>();
+            services.AddControllers();
+            AddSwaggerGen(services);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void AddSwaggerGen(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "3.0" });
+            });
+        }
+    }
+}
