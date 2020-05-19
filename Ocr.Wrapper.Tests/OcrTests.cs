005ee0d42d5dba065e3f8c99a9df693a5180d965
Mihai Petrutiu <Mihai.Petrutiu@qubiz.com>
2020-05-19 20:50:09 +0300

    Add Swagger image and use explicit classes for OcrTests

--- Ocr.Wrapper.Tests/OcrTests.cs
+++ Ocr.Wrapper.Tests/OcrTests.cs
@@ -9,6 +9,7 @@ using Ocr.Wrapper.TesseractOcr;
 using Ocr.Wrapper.GoogleOcr;
 using Ocr.Wrapper.AwsRekognitionOcr;
 using System.Configuration;
+using System.Collections.Generic;
 
 namespace Ocr.Wrapper.Tests
 {
@@ -23,7 +24,7 @@ namespace Ocr.Wrapper.Tests
 
 
             AzureOcrService azureOcrExecutor = new AzureOcrService(subscriptionKey, endpoint);
-            var result = await azureOcrExecutor.GetOcrResultAsync(@"data/abc.JPG");
+            AzureOcrResults result = await azureOcrExecutor.GetOcrResultAsync(@"data/abc.JPG");
             Assert.IsNotNull(result);
             GenericOcrResponse genericResult = result.Map();
             Assert.IsNotNull(genericResult);
@@ -33,7 +34,7 @@ namespace Ocr.Wrapper.Tests
         public async Task WindowsOcr()
         {
             WindowsOcrService windowsOcrExecutor = new WindowsOcrService();
-            var result = await windowsOcrExecutor.GetOcrResultAsync(@"data/abc.JPG", "en");
+            WindowsOcrResult result = await windowsOcrExecutor.GetOcrResultAsync(@"data/abc.JPG", "en");
             Assert.IsNotNull(result);
             GenericOcrResponse genericResult = result.Map();
             Assert.IsNotNull(genericResult);
@@ -43,7 +44,7 @@ namespace Ocr.Wrapper.Tests
         public async Task Tesseract5()
         {
             TesseractOcrService tesseractService = new TesseractOcrService();
-            var result = await tesseractService.GetOcrResultAsync(@"data/abc.JPG", "eng");
+            TesseractResponse result = await tesseractService.GetOcrResultAsync(@"data/abc.JPG", "eng");
             Assert.IsNotNull(result);
             GenericOcrResponse genericResult = result.Map();
             Assert.IsNotNull(genericResult);
@@ -55,7 +56,7 @@ namespace Ocr.Wrapper.Tests
             var apiToken = ConfigurationManager.AppSettings["googleApiToken"];
             GoogleOcrService googleOcrService = new GoogleOcrService(apiToken);
 
-            var result = await googleOcrService.GetOcrResultAsync(@"data/abc.JPG");
+            GoogleOcrResponse result = await googleOcrService.GetOcrResultAsync(@"data/abc.JPG");
             var descriptions = result.Responses.SelectMany(r => r.Annotations).ToList();
             Assert.IsNotNull(result);
             GenericOcrResponse genericResult = result.Map();
@@ -68,7 +69,7 @@ namespace Ocr.Wrapper.Tests
             var accessKey = ConfigurationManager.AppSettings["awsAccessKey"];
             var secretKey = ConfigurationManager.AppSettings["awsSecretKey"];
             AwsOcrService awsOcrService = new AwsOcrService(accessKey, secretKey);
-            var result = await awsOcrService.GetOcrResultAsync(@"data/abc.JPG");
+            AwsOcrResponse result = await awsOcrService.GetOcrResultAsync(@"data/abc.JPG");
             Assert.IsNotNull(result);
             GenericOcrResponse genericResult = result.Map();
             Assert.IsNotNull(genericResult);
@@ -81,7 +82,7 @@ namespace Ocr.Wrapper.Tests
 
             MultiOcrRunner genericOcrRunner = await new StandardMultiOcrRunnerFactory(standardOcrSettings)
                 .GetMultiOcrRunner();
-            var results = await genericOcrRunner.RunAllOcrEnginesOnImage(@"data/abc.JPG");
+            Dictionary<string, GenericOcrResponse> results = await genericOcrRunner.RunAllOcrEnginesOnImage(@"data/abc.JPG");
             Assert.IsNotNull(results);
         }
 
@@ -93,7 +94,7 @@ namespace Ocr.Wrapper.Tests
             var fullPath = Path.GetFullPath(@"..\Data\Cache\");
             MultiOcrRunner multiOcrRunner = await new StandardMultiOcrRunnerFactory(standardOcrSettings, fullPath)
                 .GetMultiOcrRunner();
-            var results = await multiOcrRunner.RunAllOcrEnginesOnImage(@"data/abc.JPG");
+            Dictionary<string, GenericOcrResponse> results = await multiOcrRunner.RunAllOcrEnginesOnImage(@"data/abc.JPG");
             Assert.IsNotNull(results);
         }
 
